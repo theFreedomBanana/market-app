443de4f27551c07acbe6b3e66a52a1af337ea326
Ben <benjamin.mahmoudi@gmail.com>
2022-02-26 22:34:12 +0100

    core: add i18Next on init with en translation file

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,30 @@
 
+import i18n from 'i18next';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { initReactI18next } from 'react-i18next';
+import enTranslations from '../res/translations/en.json';
 
 const App = () => {
 
 	return (
 		<h1>Hello world</h1>
 	)
-}
+};
 
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-);
+
+i18n.use(initReactI18next).init({
+	fallbackLng: 'en',
+	interpolation: {
+		escapeValue: false,
+	},
+	lng: 'en',
+	resources: {
+		en: enTranslations,
+	},
+}).then(() => {
+	ReactDOM.render(
+		<App />,
+		document.getElementById('root')
+	);
+});
