8048af7e4b0548058391bac475205e98ee369270
Ben <benjamin.mahmoudi@gmail.com>
2022-02-28 15:50:31 +0100

    feat: add title to itemsList and display list of items

--- src/Components/Features/Items/List/index.tsx
+++ src/Components/Features/Items/List/index.tsx
@@ -1,13 +1,22 @@
-import { createStyles, withStyles } from "@material-ui/core";
-import React, { memo, useEffect } from "react";
+import { createStyles, Grid, Paper, Typography, withStyles, WithStyles } from "@material-ui/core";
+import React, { memo, useEffect, useMemo } from "react";
+import { useTranslation } from "react-i18next";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
+import { Item } from "../../../../Classes/Item";
 import { Store } from "../../../../Store";
 import { ACTIONS } from "../../../../Store/actions";
-
+import { ItemCard } from "../Card";
 
 const styles = () => createStyles({
-	feature__container: { backgroundColor: "#FAFAFA", padding: "0 6rem" },
+	list__container: { padding: "1rem" },
+	list__title: {
+		backgroundColor: "#FAFAFA",
+		color: "#6F6F6F",
+		lineHeigh: "26px",
+		marginBottom: "1rem",
+		size: "20px",
+	},
 });
 
 const mapStateToProps = (store: Store) => ({
@@ -18,21 +27,38 @@ const mapStateToProps = (store: Store) => ({
 	},
 });
 
+type ItemsListProps = { dispatch: Dispatch; store: any } & WithStyles<typeof styles>;
+
 export const ItemsList = connect(mapStateToProps)(
 	withStyles(styles)(
 		memo(
-			({ dispatch }: { dispatch: Dispatch }) => {
+			({ classes, dispatch, store }: ItemsListProps) => {
+				const { t } = useTranslation();
 
 				useEffect(
 					() => {
-						dispatch({ type: ACTIONS.FETCH_REQUESTED });
+						dispatch({ data: { limit: 16 }, type: ACTIONS.FETCH_REQUESTED });
 					},
 					[dispatch],
 				);
 
+				const items: Item[] = useMemo(
+					() => Object.values(store?.information.item),
+					[store?.information.item],
+				);
+
 				return (
 					<>
-						<h1>foo</h1>
+						<Typography className={classes.list__title} variant="h4">{t("Feature:Items:List:products")}</Typography>
+						<Paper className={classes.list__container} elevation={2}>
+							<Grid container spacing={2}>
+								{items.map((item) => (
+									<Grid item key={item.slug} md={3}>
+										<ItemCard item={item} />
+									</Grid>
+								))}
+							</Grid>
+						</Paper>
 					</>
 				);
 			},
