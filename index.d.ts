0cad29e276d1656c18b3bef08642954e33fe4c41
Ben <benjamin.mahmoudi@gmail.com>
2022-03-01 10:29:57 +0100

    doc: add type declaration for ".svg" files

--- index.d.ts
+++ index.d.ts
@@ -12,3 +12,8 @@ declare module "*.png" {
 	const value: string;
 	export = value;
 }
+
+declare module "*.svg" {
+	const value: React.ElementType<React.ComponentPropsWithRef<"svg">>;
+	export = value;
+}
